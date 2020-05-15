7fbc6a023c9888d84f561e618c17e510f1c4f049
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-14 20:36:37 -0700

    set header type

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -13,10 +13,43 @@ export default class Header extends Component {
             <br />
             BASCHE
           </h1>
-          <Typist.Delay ms={500} />
-          <h3 className='position'>
-            FULL STACK WEB DEVLOPER
-          </h3>
+          {/* <Typist.Delay ms={500} /> */}
+          <h3 className='position'>FULL STACK WEB DEVLOPER</h3>
+          <div className='skills'>
+            <p>REACT</p>
+            <Typist.Backspace count={5} delay={200} />
+            <p>JAVASCRIPT</p>
+            <Typist.Backspace count={10} delay={200} />
+            <p>NODEJS</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>EXPRESS</p>
+            <Typist.Backspace count={7} delay={200} />
+            <p>MONOGO</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>HTML</p>
+            <Typist.Backspace count={4} delay={400} />
+            <p>CSS</p>
+            <Typist.Backspace count={3} delay={400} />
+            <p>DJANGO</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>
+              REACT
+              <br />
+              JAVASCRIPT
+              <br />
+              NODEJS
+              <br />
+              EXPRESS
+              <br />
+              MONGO
+              <br />
+              HTML
+              <br />
+              CSS
+              <br />
+              DJANGO
+            </p>
+          </div>
         </Typist>
       </div>
     );
