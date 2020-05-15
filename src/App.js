a729d33c564a82243840ae0620748225e3a6575f
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-14 20:16:32 -0700

    add components on App page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 import Header from './Components/Header';
+import AboutMe from './Components/About Me'
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
+      <Navbar />
       <Header />
+      <AboutMe />
+      <Projects />
+      <Contact />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className='tags'>
+        <a href='#'>About Me</a>
+        <a href='#'>Projects</a>
+        <a href='#'>Contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
