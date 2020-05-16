8632d0e3286711d425210d3d0113ebe58b1f47da
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-16 15:32:19 -0700

    add video bg behind header type

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,62 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typist from 'react-typist';
 
 import sample from '../../Assets/WebsiteBG.mp4';
 
-import './style.css';
+import classes from './BackgroundVideo.module.css';
+// import './style.css';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <>
-        <div className='head'>
+const BackgroundVideo = () => {
+  // let videoSource = '../../Assets/WebsiteBG.mp4'
+  return (
+    <div>
+      <video autoPlay='autoplay' loop='loop' muted className={classes.Video}>
+        <source src={sample} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className={classes.Content}>
+        <div className={classes.SubContent}>
           <Typist cursor={{ show: false }}>
-            <h1 className='name'>
-              <Typist.Delay ms={1000} />
-              JOSHUA
+          <h1 className='name'>
+            <Typist.Delay ms={1000} />
+            JOSHUA
+            <br />
+            BASCHE
+          </h1>
+          <h3 className='position'>FULL STACK WEB DEVLOPER</h3>
+          <div className='skills'>
+            <p>REACT</p>
+            <Typist.Backspace count={5} delay={200} />
+            <p>JAVASCRIPT</p>
+            <Typist.Backspace count={10} delay={200} />
+            <p>NODEJS</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>EXPRESS</p>
+            <Typist.Backspace count={7} delay={200} />
+            <p>MONOGO</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>HTML</p>
+            <Typist.Backspace count={4} delay={400} />
+            <p>CSS</p>
+            <Typist.Backspace count={3} delay={400} />
+            <p>DJANGO</p>
+            <Typist.Backspace count={6} delay={200} />
+            <p>
+              REACT
               <br />
-              BASCHE
-            </h1>
-            {/* <Typist.Delay ms={500} /> */}
-            <h3 className='position'>FULL STACK WEB DEVLOPER</h3>
-            <div className='skills'>
-              <p>REACT</p>
-              <Typist.Backspace count={5} delay={200} />
-              <p>JAVASCRIPT</p>
-              <Typist.Backspace count={10} delay={200} />
-              <p>NODEJS</p>
-              <Typist.Backspace count={6} delay={200} />
-              <p>EXPRESS</p>
-              <Typist.Backspace count={7} delay={200} />
-              <p>MONOGO</p>
-              <Typist.Backspace count={6} delay={200} />
-              <p>HTML</p>
-              <Typist.Backspace count={4} delay={400} />
-              <p>CSS</p>
-              <Typist.Backspace count={3} delay={400} />
-              <p>DJANGO</p>
-              <Typist.Backspace count={6} delay={200} />
-              <p>
-                REACT
-                <br />
-                JAVASCRIPT
-                <br />
-                NODEJS
-                <br />
-                EXPRESS
-                <br />
-                MONGO
-                <br />
-                HTML
-                <br />
-                CSS
-                <br />
-                DJANGO
-              </p>
-            </div>
+              JAVASCRIPT
+              <br />
+              NODEJS
+              <br />
+            EXPRESS
+            <br />
+            MONGO
+            <br />
+            HTML
+            <br />
+            CSS
+            <br />
+            DJANGO
+          </p>
+          </div>
           </Typist>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
