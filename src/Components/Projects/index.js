a30bc4e3ea318da6db561865e1a7c50031e30b57
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-18 15:47:40 -0700

    add first card

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -6,7 +6,7 @@ import './style.css';
 
 const Projects = () => {
   return (
-    <div className='about'>
+    <div className='projects'>
       <ScrollAnimation
         delay={500}
         duration={3}
@@ -14,7 +14,36 @@ const Projects = () => {
         animateIn='fadeIn'
         animateOut='fadeOut'
       >
-        <h1 className='title'>PROJECTS</h1>
+        <div className='title'>
+          <h1>PROJECTS</h1>
+        </div>
+        <div className='wrapper'>
+          <div className='card'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SL1500_.jpg'
+              alt='poster'
+            ></img>
+            <div className='descriptions'>
+              <h1>John Wick</h1>
+              <p>
+                After gunning down a member of the High Table -- the shadowy
+                international assassin's guild -- legendary hit man John Wick
+                finds himself stripped of the organization's protective
+                services. Now stuck with a $14 million bounty on his head, Wick
+                must fight his way through the streets of New York as he becomes
+                the target of the world's most ruthless killers.
+              </p>
+              <a
+                href='https://github.com/HuemanTarget'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-github-square' />
+                <h4 className='link'>GitHub</h4>
+              </a>
+            </div>
+          </div>
+        </div>
       </ScrollAnimation>
     </div>
   );
