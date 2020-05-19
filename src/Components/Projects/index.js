1f8f7a4bc9a4fdfb2ebf944d698de66219b7af4c
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-18 18:24:35 -0700

    fix projects css height

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -15,7 +15,7 @@ const Projects = () => {
         animateOut='fadeOut'
       >
         <div className='title'>
-          <h1>PROJECTS</h1>
+        <h1>PROJECTS</h1>
         </div>
         <div className='wrapper'>
           <div className='card'>
@@ -30,12 +30,110 @@ const Projects = () => {
                 murdered. Made with ReactJS, Firebase, and Firestore.
               </p>
               <a
-                href='https://github.com/HuemanTarget'
+                href='https://github.com/HuemanTarget/interACTive-theater-app'
                 target='_blank'
                 rel='noopener noreferrer'
               >
                 <i className='fa fa-github-square' />
                 <h4 className='link'>GitHub</h4>
+              </a>
+              <a
+                href='https://interactive-theatrical-app.firebaseapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-laptop' />
+                <h4 className='link'>Live Demo</h4>
+              </a>
+            </div>
+          </div>
+          <div className='card'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SL1500_.jpg'
+              alt='poster'
+            ></img>
+            <div className='descriptions'>
+              <h1>Red Cup Games</h1>
+              <p>
+                A message board for people to post about their drinking games or
+                find new ones from other users or use the chat room to talk to
+                people directly. Made with Django, Python, and AWS.
+              </p>
+              <a
+                href='https://github.com/HuemanTarget/red-cup-games'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-github-square' />
+                <h4 className='link'>GitHub</h4>
+              </a>
+              <a
+                href='https://redcupgames.herokuapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-laptop' />
+                <h4 className='link'>Live Demo</h4>
+              </a>
+            </div>
+          </div>
+          <div className='card'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SL1500_.jpg'
+              alt='poster'
+            ></img>
+            <div className='descriptions'>
+              <h1>Red Cup Games</h1>
+              <p>
+                A message board for people to post about their drinking games or
+                find new ones from other users or use the chat room to talk to
+                people directly. Made with Django, Python, and AWS.
+              </p>
+              <a
+                href='https://github.com/HuemanTarget/red-cup-games'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-github-square' />
+                <h4 className='link'>GitHub</h4>
+              </a>
+              <a
+                href='https://redcupgames.herokuapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-laptop' />
+                <h4 className='link'>Live Demo</h4>
+              </a>
+            </div>
+          </div>
+          <div className='card'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SL1500_.jpg'
+              alt='poster'
+            ></img>
+            <div className='descriptions'>
+              <h1>Red Cup Games</h1>
+              <p>
+                A message board for people to post about their drinking games or
+                find new ones from other users or use the chat room to talk to
+                people directly. Made with Django, Python, and AWS.
+              </p>
+              <a
+                href='https://github.com/HuemanTarget/red-cup-games'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-github-square' />
+                <h4 className='link'>GitHub</h4>
+              </a>
+              <a
+                href='https://redcupgames.herokuapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fa fa-laptop' />
+                <h4 className='link'>Live Demo</h4>
               </a>
             </div>
           </div>
