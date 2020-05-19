873b3ca61488ce6ca382c8b9702eb57add742c05
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-05-18 21:58:12 -0700

    add contact info

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'animate.css/animate.min.css';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { List, ListItemContent, ListItem } from 'react-mdl';
 
 import './style.css';
 
 const Contact = () => {
   return (
-    <div className='about'>
+    <div className='contact'>
       <ScrollAnimation
         delay={500}
         duration={3}
@@ -15,6 +16,24 @@ const Contact = () => {
         animateOut='fadeOut'
       >
         <h1 className='title'>CONTACT</h1>
+        <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft'>
+          <div>
+            <a href='tel:3108067483' class='button'>
+              <div class='qube'>
+                <div class='front'>(310)806-7483</div>
+                <div class='back'>Phone Number</div>
+              </div>
+            </a>
+          </div>
+          {/* <div>
+            <a href='mailto:joshbasche@live.com' class='button'>
+              <div class='qube'>
+                <div class='front'>joshbasche@live.com</div>
+                <div class='back'>Email</div>
+              </div>
+            </a>
+          </div> */}
+        </ScrollAnimation>
       </ScrollAnimation>
     </div>
   );
